8a98984e6c9bde62b189681524000986f37e7b22
Marcelo <marcelodanelon11@gmail.com>
2022-07-23 20:58:38 -0300

    Projeto criado

---
+++ game_xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace game_xadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
